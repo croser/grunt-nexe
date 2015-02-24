8542ad7b29772083b20409a080160a3309b9c73d
croser <j.kuster@gmx.at>
2015-02-24 15:37:21 +0000

    making temp folder configurable

--- src/nexe.coffee
+++ src/nexe.coffee
@@ -5,13 +5,13 @@ nexe = require 'nexe'
 module.exports = (grunt)->
   grunt.registerTask 'nexe', 'create single executable file by nexe', ->
     done = @async()
-    {input, output, flags, version} = grunt.config('nexe')
+    {input, output, flags, version, temp} = grunt.config('nexe')
     option =
       input: path.join(process.cwd(), input)
       output: path.join(process.cwd(), output)
       flags: if flags isnt undefined then flags else false
       nodeVersion: if version isnt undefined then version else 'latest'
-      nodeTempDir: '/tmp'
+      nodeTempDir: if temp isnt undefined then temp else '/tmp'
     ncp path.join(process.cwd(), 'node_modules'), path.join(path.dirname(option.input), 'node_modules'), (err)->
       nexe.compile option, (err)->
         grunt.log.error err if err?
--- tasks/nexe.js
+++ tasks/nexe.js
@@ -8,15 +8,15 @@ nexe = require('nexe');
 
 module.exports = function(grunt) {
   return grunt.registerTask('nexe', 'create single executable file by nexe', function() {
-    var done, flags, input, option, output, version, _ref;
+    var done, flags, input, option, output, temp, version, _ref;
     done = this.async();
-    _ref = grunt.config('nexe'), input = _ref.input, output = _ref.output, flags = _ref.flags, version = _ref.version;
+    _ref = grunt.config('nexe'), input = _ref.input, output = _ref.output, flags = _ref.flags, version = _ref.version, temp = _ref.temp;
     option = {
       input: path.join(process.cwd(), input),
       output: path.join(process.cwd(), output),
       flags: flags !== void 0 ? flags : false,
       nodeVersion: version !== void 0 ? version : 'latest',
-      nodeTempDir: '/tmp'
+      nodeTempDir: temp !== void 0 ? temp : '/tmp'
     };
     return ncp(path.join(process.cwd(), 'node_modules'), path.join(path.dirname(option.input), 'node_modules'), function(err) {
       return nexe.compile(option, function(err) {
